8af13470a7475cf2a756a4f3df14a9b22f9907a1
Piotrek Kluziok <piotrkluziok@gmail.com>
2022-06-25 17:15:13 +0200

    First application ready

---
+++ App-1-Geometry_Game/main.py
@@ -0,0 +1,21 @@
+import geometry_game_v1
+# import turtle
+#
+# # Creates Canvas
+# my_turtle = turtle.Turtle()
+#
+# # Go to a certain coordinates
+# my_turtle.penup()
+# my_turtle.goto(100, 100)
+# my_turtle.pendown()
+#
+# my_turtle.forward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.right(90)
+# my_turtle.forward(100)
+# my_turtle.right(90)
+#
+# turtle.done()  # The Canvas stays after compiling
